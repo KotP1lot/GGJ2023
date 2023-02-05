345989cc4881ff616b0db05c76d69d70e5f5730f
Bohdan Dzyubanchuk <tahkictik123@gmail.com>
2023-02-05 04:41:44 +0200

    SomeChanges

--- Assets/Scripts/Media/PausePanel.cs
+++ Assets/Scripts/Media/PausePanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -7,16 +8,21 @@ public class PausePanel : MonoBehaviour
 {
     public AudioMixerGroup mixerMusic, mixerSound;
     [SerializeField] private PauseController pause;
+    private bool isMainMenu = false;
+    private void Start()
+    {
+        isMainMenu = !GameObject.Find("SettingsWindow").IsUnityNull();
+    }
     public void ChangeMusicVolume(float volume)
     {
-        if(pause.IsActive)
+        if((pause != null && pause.IsActive) || isMainMenu)
         {
             mixerMusic.audioMixer.SetFloat("MusicVolume", Mathf.Lerp(-80, 0, volume));
         }
     } 
     public void ChangeSoundVolume(float volume)
     {
-        if (pause.IsActive)
+        if ((pause != null && pause.IsActive) || isMainMenu)
         {
             mixerSound.audioMixer.SetFloat("SoundsVolume", Mathf.Lerp(-80, 0, volume));
         } 
